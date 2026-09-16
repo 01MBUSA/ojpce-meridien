ebb2a362082c6a3384a221194f3ee6c4409a8e0f
01MBUSA <ojpcemeridien@gmail.com>
2026-09-16 07:07:11 +0200

    Fix image mapping logic in cms-content.js
    Update image mapping to ensure only valid images are included.

--- js/cms-content.js
+++ js/cms-content.js
@@ -56,7 +56,7 @@
         .map(item => {
           const images = [
             item.image,
-            ...(item.images || []).map(photo => photo)
+           ...(item.images || []).map(photo => photo && photo.image)
           ].filter(Boolean).map(asset);
 
           const photos = images.map((image, index) => `
